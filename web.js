b97895612b69c495e6801a9470fbc7fee53d6689
bob roony <bobroony@bobroony.com>
2013-07-07 09:11:46 -0700

    response.send buf

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
   var buf = fs.readFileSync('index.html');
-  buf.toString();
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
